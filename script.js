202a66b3457dbcfcd448419d59cd11662eeae538
edgars-roze <klopojed.son@gmail.com>
2025-11-13 11:06:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 document.getElementById("play").addEventListener("click", function() {
-  
+  await wait(100);
 });
 
 document.getElementById("stop").addEventListener("click", function() {
