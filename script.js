34e4fc1ae45f1e868769f2ba03a133010ee7b608
edgars-roze <klopojed.son@gmail.com>
2025-11-13 11:12:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let bpm = 140;
+const input = document.getElementById("bpm");
+
 function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+input.addEventListener("input", function() {
+  bpm = Number(input.value);
+});
+
 document.getElementById("play").addEventListener("click", function() {
-  await wait(100);
+  console.log("step");
+  await wait(60000 / bpm);
 });
 
 document.getElementById("stop").addEventListener("click", function() {
